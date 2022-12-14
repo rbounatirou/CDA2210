9daa60759b027329fb01d036e95da4db0ad53e2e
rbounatirou <92740209+rbounatirou@users.noreply.github.com>
2022-12-14 16:27:16 +0100

    Add object project

---
+++ objet/Dirctionnaire/Dirctionnaire/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Dirctionnaire
+{
+    internal class Program
+    {
+
+        enum PneuPosition
+        {
+            Avant_gauche,
+            Avant_droit,
+            Arriere_gauche,
+            Arriere_droit
+        };
+
+        static void Main(string[] args)
+        {
+            
+
+            Dictionary<PneuPosition,string> myDic= new Dictionary<PneuPosition, string>();
+            myDic.Add(PneuPosition.Avant_gauche, "testAG");
+            myDic.Add(PneuPosition.Avant_droit, "testAD");
+            myDic.Add(PneuPosition.Arriere_gauche, "testRG");
+            myDic.Add(PneuPosition.Arriere_droit, "testRD");
+            Console.WriteLine(myDic[PneuPosition.Arriere_droit]);
+
+        }
+    }
+}
