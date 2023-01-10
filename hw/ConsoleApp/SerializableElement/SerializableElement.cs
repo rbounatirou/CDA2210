0c0ef955ec3507fec378cd0c78e8abbc00344c00
rbounatirou <92740209+rbounatirou@users.noreply.github.com>
2023-01-10 21:34:03 +0100

    add binary serialize into hw folder

---
+++ hw/ConsoleApp/SerializableElement/SerializableElement.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.Serialization;
+
+namespace SerializableElements
+{
+    [Serializable]
+    public class SerializableElement : ISerializable
+    {
+        private string nom;
+        private string prenom;
+        private int age;
+        private string pseudo;
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; }
+        public int Age { get => age; set => age = value; }
+        public string Pseudo { get => pseudo; set => pseudo = value; }
+
+        public SerializableElement(string nom, string prenom, int age, string pseudo)
+        {
+            this.nom = nom;
+            this.prenom = prenom;
+            this.age = age;
+            this.pseudo = pseudo;
+        }
+
+        public SerializableElement(SerializationInfo info, StreamingContext context)
+        {
+            nom = (string)info.GetValue("nom", typeof(string));
+            prenom = (string)info.GetValue("prenom", typeof(string));
+            pseudo = (string)info.GetValue("pseudo", typeof(string));
+            age = (int)info.GetValue("age", typeof(int));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("nom", nom, typeof(string));
+            info.AddValue("prenom", prenom, typeof(string));
+            info.AddValue("age", age, typeof(int));
+            info.AddValue("pseudo",pseudo, typeof(string));
+        }
+    }
+}
---
+++ hw/ConsoleApp/SerializationBinaire/Program.cs
@@ -0,0 +1,43 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using SerializableElements;
+
+namespace SerializationBinaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "dataStuff.myData";
+
+            // Use a BinaryFormatter or SoapFormatter.
+            IFormatter formatter = new BinaryFormatter();
+            //IFormatter formatter = new SoapFormatter();
+
+            string tmp = SerializeItem(formatter); // Serialize an instance of the class.
+            DeserializeItem(tmp, formatter); // Deserialize the instance.
+            Console.WriteLine("Done");
+            Console.ReadLine();
+        }
+
+        public static string SerializeItem(IFormatter formatter)
+        {
+            // Create an instance of the type and serialize it.
+            SerializableElement t = new SerializableElement("rody","bou",29,"rody");
+
+            MemoryStream s = new MemoryStream();
+            formatter.Serialize(s, t);
+            string rt = System.Text.Encoding.Unicode.GetString(s.ToArray());
+            s.Close();
+            return rt;
+        }
+
+        public static void DeserializeItem(string file,IFormatter formatter)
+        {
+            MemoryStream s = new MemoryStream(System.Text.Encoding.Unicode.GetBytes(file));
+            SerializableElement t = (SerializableElement)formatter.Deserialize(s);
+            Console.WriteLine(t.Age + " ans");
+        }
+    }
+}
