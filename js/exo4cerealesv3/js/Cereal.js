9a0663ff8e77aae8d00f74897fa2b6a1e88f4165
BOUNATIROU Rodolphe <RBounatirou@arfp.asso.fr>
2023-06-13 14:39:53 +0200

    Add sort feature to cereals

--- js/exo4cerealesv3/js/Cereal.js
+++ js/exo4cerealesv3/js/Cereal.js
@@ -1,16 +1,15 @@
 class Cereal{
-    constructor(card){
-        Object.assign(this, card);
+    constructor(){
+        this.id = 0;
+        this.name = "";
+        this.calories = 0;
+        this.protein = 0;
+        this.sodium = 0;
+        this.fiber = 0;
+        this.carbo = 0;
+        this.sugars = 0;
+        this.potass = 0;
+        this.vitamins = 0;
+        this.rating = 0.0;
     }
-
-    getKeys(){
-        return Object.keys(this);
-    }
-
-    getValues(){
-        return Object.values(this);
-    }
-
 }
-
-export { Cereal };
