288526f9b40a71869a7d51760e1c0c5c9dd87f90
BOUNATIROU Rodolphe <RBounatirou@arfp.asso.fr>
2023-07-18 16:56:01 +0200

    Command exemple

---
+++ Pattern/ExempleCommand/ExempleCommand/Personnage.cs
@@ -0,0 +1,31 @@
+﻿namespace ExempleCommand
+{
+
+    // LE PERSONNAGE JOUERA LE ROLE DE RECEIVER
+    public class Personnage
+    {
+        // LA POSITION DU JOUEUR EST DETERMINEE PAR SES COORDONEED
+        private int x;
+        private int y;        
+        
+        public Personnage(int _x, int _y)
+        {
+            this.x = _x;
+            this.y = _y;
+        }
+
+        public void DeplacerAGauche(int nbPx)
+        {
+            this.x -= nbPx;
+        }
+
+        public void DeplacerEnHaut(int nbPx)
+        {
+            this.y -= nbPx;
+        }
+
+        public void DeplacerADroite(int nbPx) => DeplacerAGauche(-nbPx);
+
+        public void DeplacerEnBas(int nbPx) => DeplacerEnHaut(-nbPx);
+    }
+}
